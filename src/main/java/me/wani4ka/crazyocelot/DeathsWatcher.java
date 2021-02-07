f4cfcb11b9fb17510c33974ead7eadef8a914bdb
Wani4ka <contact.wani4ka@gmail.com>
2021-02-07 16:34:31 +0300

    Simplified killer check. Spawning baby ocelot if baby zombie was killed

--- src/main/java/me/wani4ka/crazyocelot/DeathsWatcher.java
+++ src/main/java/me/wani4ka/crazyocelot/DeathsWatcher.java
@@ -3,9 +3,10 @@ package me.wani4ka.crazyocelot;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Ocelot;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 @RequiredArgsConstructor
@@ -16,10 +17,12 @@ public class DeathsWatcher implements Listener {
 	@EventHandler
 	public void onZombieDied(EntityDeathEvent e) {
 		if (e.getEntityType() != EntityType.ZOMBIE) return;
-		val lastDmg = e.getEntity().getLastDamageCause();
-		if (!(lastDmg instanceof EntityDamageByEntityEvent)) return;
-		if (((EntityDamageByEntityEvent) lastDmg).getDamager().getType() == EntityType.PLAYER)
-			plugin.spawnOcelot(e.getEntity().getLocation());
+		val killer = e.getEntity().getKiller();
+		if (killer != null) {
+			val ocelot = plugin.spawnOcelot(e.getEntity().getLocation());
+			if (((Zombie) e.getEntity()).isBaby() && ocelot != null)
+				((Ocelot) ocelot).setBaby();
+		}
 	}
 
 }
